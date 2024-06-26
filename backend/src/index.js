88cf0f6b8151a4f88833ef3109a4d846e710d341
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-26 20:43:13 +0530

    installing libraries like bcryptjs, jsonwebtoken and express-validator. Connected mongoDB databse, implemented password hashing

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('../config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+app.get('/', (req, res) => res.send({ message: 'Welcome to AI-Powered Career Counseling Platform!' }));
+
+// Define Routes
+app.use('/api/users', require('../routes/users'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
