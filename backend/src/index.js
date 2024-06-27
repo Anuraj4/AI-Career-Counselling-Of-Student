a0b0c629b8f7ad8620638721735991dd33ed423e
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-27 13:20:41 +0530

    Created authentication middleware, implemented login route

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ app.get('/', (req, res) => res.send({ message: 'Welcome to AI-Powered Career Cou
 
 // Define Routes
 app.use('/api/users', require('../routes/users'));
+app.use('/api/auth', require('../routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
